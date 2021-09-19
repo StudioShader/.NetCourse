1e47d422604a420bf5ec658f25367fcd0e2711e1
Иван Оглоблин <studioshader2018@gmail.com>
2021-09-19 14:24:51 +0300

    StackWithMinElement

---
+++ StackWithMinElement/StackWithMinElement/MinStack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackWithMinElement
+{
+    class MinStack<T> : IEnumerable<(T, bool)> where T : IComparable
+    {
+        private Stack<(T,bool)> Array;
+        private T MinElement;
+        public T Get()
+        {
+            return Array.Peek().Item1;
+        }
+
+        public T GetMinElement()
+        {
+            if (Array.Count == 0)
+            {
+                throw default(Exception);
+            }
+            return MinElement;
+        }
+        public MinStack()
+        {
+
+            Array = new Stack<(T, bool)>(0);
+            return;
+
+        }
+        public MinStack(Stack<T> array)
+        {
+            if (array.Count == 0)
+            {
+                Array = new Stack<(T, bool)>(0);
+                return;
+            }
+            Stack<T> Array2 = array;
+            for(int i = 0; i < Array2.Count; i++)
+            {
+                T some = Array2.Pop();
+                this.Push(some);
+            }
+        }
+        public T Pop()
+        {
+            T some = Array.Pop().Item1;
+            MinElement = Array.Peek().Item1;
+            foreach ((T, bool) elem in Array)
+            {
+                if (elem.Item2 && elem.Item1.CompareTo(MinElement) < 0)
+                {
+                    MinElement = elem.Item1;
+                }
+            }
+            return some;
+        }
+
+        public void Push(T element)
+        {
+            if (Array.Count == 0)
+            {
+                Array.Push((element, true));
+                MinElement = element;
+                return;
+            }
+            if (element.CompareTo(MinElement) < 0)
+            {
+                Array.Push((element, true));
+                MinElement = element;
+                return;
+            }
+            Array.Push((element, false));
+        }
+
+        public int size()
+        {
+            return Array.Count();
+        }
+
+        public IEnumerator<(T, bool)> GetEnumerator()
+        {
+            return Array.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Array.GetEnumerator();
+        }
+    }
+}
---
+++ StackWithMinElement/StackWithMinElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackWithMinElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MinStack<int> stack = new MinStack<int>();
+            Console.WriteLine(stack.size());
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(2);
+            stack.Push(7);
+            stack.Push(1);
+            stack.Push(9);
+            Console.WriteLine(stack.GetMinElement());
+            Console.WriteLine(stack.size());
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine(stack.GetMinElement());
+            Console.WriteLine(stack.size());
+            Console.ReadLine();
+            
+        }
+    }
+}
