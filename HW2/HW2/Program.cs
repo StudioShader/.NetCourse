5a80fa71cdc50a2fb38dd5df665f18ff8452afc8
Иван Оглоблин <studioshader2018@gmail.com>
2021-09-20 21:54:43 +0300

    +inheritage on structs

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -4,9 +4,44 @@ namespace HW2
 {
     class Program
     {
+        struct SomeBase
+        {
+            public int index { get; set; }
+            public SomeBase(int somInt)
+            {
+                index = somInt;
+            }
+            public int GetIndex()
+            {
+                return index;
+            }
+        }
+        struct Derived
+        {
+            public SomeBase baseClass;
+
+            public string Name { get; set; }
+
+            public Derived(SomeBase based){
+                baseClass = based;
+                Name = "Knot";
+            }
+            public void ChangeName()
+            {
+                Name += baseClass.index;
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // inheritage woth structs
+            SomeBase A = new SomeBase(4);
+            Derived B = new Derived(A);
+            B.baseClass.index = 4;
+            B.ChangeName();
+            Console.WriteLine(B.Name);
+
             MyHashTable<int> Mass = new MyHashTable<int>(10);
             Mass.Add(3);
             Mass.Add(5);
@@ -18,6 +53,7 @@ namespace HW2
             }
             Mass.Remove(6);
             Console.WriteLine(Mass.Contains(6));
+
             ImmutableType my = ImmutableType.ConstructArticle("some name", "article");
             ImmutableType another = my.ChangeArticleTo("some other article");
             Console.WriteLine("my: " + my.Name + " article: " + my.Article);
