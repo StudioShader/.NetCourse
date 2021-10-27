2c42519d1e39aaea82c3ed97fc3148b5b5701228
unknown <dmitry.shalymov@gmail.com>
2021-10-27 12:53:05 +0300

    review 7

--- Hw7/Hw7/Program.cs
+++ Hw7/Hw7/Program.cs
@@ -24,28 +24,37 @@ namespace Hw7
             list.Add(new People { name = "some6", age = 2 });
             list.Add(new People { name = "some8", age = 2 });
             string delim = " | ";
+            // Для конкатенации коллекции строк с указанным разделителем лучше использовать String.Join
             String all = list
                     .Skip(3)
                     .Select((p, i) => (i == (list.Count-4)) ? p.name : (p.name + delim))
                     .Aggregate((longest, name) => longest + name);
             Console.WriteLine(all);
+
             // Second Task
             int number = list.Where((n, i) => n.name.Length > i).Count();
             Console.WriteLine(number);
+
             //Third
             string s = "Это что же получается: ходишь, ходишь в школу, а потом бац - вторая смена";
             string[] split = s.Split(':', ',', ' ', '-');
-            var another = split.Where(a => a.Length != 0).GroupBy(str => str.Length).OrderBy(a => a.Key);
+            // задание: отсортировать группы по убыванию количества элементов в каждой группе
+            var another = split.Where(a => a.Length != 0).GroupBy(str => str.Length).OrderByDescending(a => a.Count()).ThenByDescending(a => a.Key);
             int i = 0; 
             foreach(var a in another)
             {
                 i++;
-                Console.WriteLine("Группа " + i + " Длина: " + a.Count() + " Количество: " + a.Key);
+                // используйте интерполяцию строк - работает быстрее и эффективнее по памяти!
+                // спутаны местами длина и количество
+                Console.WriteLine($"Группа {i} Длина: {a.Key} Количество: {a.Count()}");
+                // давайте "говорящие" имена переменным.
+                // код должен быть самодокументирующимся
                 foreach(var b in a)
                 {
                     Console.WriteLine(b);
                 }
             }
+
             //Fourth
             string s2 = "This dog eats too much vegetables after lunch";
             string[] split2 = s2.Split(':', ',', ' ', '-');
